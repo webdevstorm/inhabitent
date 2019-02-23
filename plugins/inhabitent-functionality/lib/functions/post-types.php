51e767e840ba25b8265f1bcc6b1f82b54a1448b3
Sage <sagevanhatten4@gmail.com>
2019-02-22 16:35:30 -0800

    mistakenly delayed commit

---
+++ plugins/inhabitent-functionality/lib/functions/post-types.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * POST TYPES
+ *
+ * @link  http://codex.wordpress.org/Function_Reference/register_post_type
+ */
+
+// Add your custom post types here...
+// Register Custom Post Type
+function inhabitent_product() {
+
+	$labels = array(
+		'name'                  => 'Products',
+		'singular_name'         => 'Product',
+		'menu_name'             => 'Products',
+		'name_admin_bar'        => 'Product',
+		'archives'              => 'Product Archives',
+		'attributes'            => 'Product Attributes',
+		'parent_item_colon'     => 'Parent Product:',
+		'all_items'             => 'All Products',
+		'add_new_item'          => 'Add New Product',
+		'add_new'               => 'Add New Product',
+		'new_item'              => 'New Product',
+		'edit_item'             => 'Edit Product',
+		'update_item'           => 'Update Product',
+		'view_item'             => 'View Product',
+		'view_items'            => 'View Products',
+		'search_items'          => 'Search Product',
+		'not_found'             => 'Not found',
+		'not_found_in_trash'    => 'Not found in Trash',
+		'featured_image'        => 'Featured Image',
+		'set_featured_image'    => 'Set featured image',
+		'remove_featured_image' => 'Remove featured image',
+		'use_featured_image'    => 'Use as featured image',
+		'insert_into_item'      => 'Insert into Product',
+		'uploaded_to_this_item' => 'Uploaded to this Product',
+		'items_list'            => 'Items list',
+		'items_list_navigation' => 'Items list navigation',
+		'filter_items_list'     => 'Filter Products list',
+	);
+	$args = array(
+		'label'                 => 'Product',
+		'description'           => 'Inhabitent Products',
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail', 'revisions' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-cart',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+        'capability_type'       => 'page',
+        'show_in_rest'          => true,
+        'template_lock'         => 'all',
+        'template'              => array(
+            array( 'core/paragraph', array(
+                'placeholder' => 'Description of the product',
+            ) ),
+        ),
+	);
+	register_post_type( 'product', $args );
+
+}
+add_action( 'init', 'inhabitent_product', 0 );
